67f4b5dae34f94d0d234f2d52d1fd1abfb6cf896
kirang89 <kiran.daredevil@gmail.com>
2013-09-16 15:19:50 +0530

    Added instructions for package init

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,3 +1,11 @@
 #! ../env/bin/python
 
+#
+# Add all your necessary imports here. Typically, __init__.py should only contain imports and not code
+#
+
+# The package version
 __version__ = '0.0.1'
+
+# Defines the list of module names to be imported, when "from package import *" is encountered
+__all__ = []
