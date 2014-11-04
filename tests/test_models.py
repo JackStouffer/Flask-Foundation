ceab34a65fa92d8c2054df023185190b5438bcc1
Jack Stouffer <jack@jackstouffer.com>
2014-11-04 16:04:09 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -15,11 +15,18 @@ class TestModels:
         db.session.remove()
         db.drop_all()
 
-    def test_user(self):
+    def test_user_save(self):
+        """ Test Saving the user model """
+        admin = User('admin', 'supersafepassword')
+        db.session.add(admin)
+        db.session.commit()
+        
+        user = User.query.filter_by(username="admin").first()
+        assert user is not None
+        
+    def test_user_password(self):
+        """ Test password hashing and checking """
         admin = User('admin', 'supersafepassword')
 
         assert admin.username == 'admin'
         assert admin.check_password('supersafepassword')
-
-        db.session.add(admin)
-        db.session.commit()
