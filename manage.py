59e623be435365ee26e3d2280106505ccdf81b5f
Jack Stouffer <jack@jackstouffer.com>
2016-02-22 19:32:00 -0500

    Fix Issue #9

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ from appname.models import db, User
 # default to dev config because no one should use this in
 # production anyway
 env = os.environ.get('APPNAME_ENV', 'dev')
-app = create_app('appname.settings.%sConfig' % env.capitalize(), env=env)
+app = create_app('appname.settings.%sConfig' % env.capitalize())
 
 manager = Manager(app)
 manager.add_command("server", Server())
