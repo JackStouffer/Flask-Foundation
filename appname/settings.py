5a6d72a23705839bd7ab1aef1327c8ed61e8347c
David Schlesinger <lefty@shugendo.org>
2016-09-19 21:26:31 -0700

    Changes to support Flask-Migrate in framework

--- appname/settings.py
+++ appname/settings.py
@@ -1,14 +1,19 @@
 import tempfile
+import os
 db_file = tempfile.NamedTemporaryFile()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = 'REPLACE ME'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
 
 
 class ProdConfig(Config):
     ENV = 'prod'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, '../database.sqlite')
 
     CACHE_TYPE = 'simple'
 
@@ -18,7 +23,8 @@ class DevConfig(Config):
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, '../database-dev.sqlite')
 
     CACHE_TYPE = 'null'
     ASSETS_DEBUG = True
@@ -29,7 +35,8 @@ class TestConfig(Config):
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_file.name
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + db_file
     SQLALCHEMY_ECHO = True
 
     CACHE_TYPE = 'null'
