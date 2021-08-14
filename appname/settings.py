a68d04a58af5f3e81a58bb41fb650aafb86079ca
Jack Stouffer <jack@jackstouffer.com>
2021-08-14 17:48:00 -0400

    Update Flask

--- appname/settings.py
+++ appname/settings.py
@@ -4,34 +4,39 @@ db_file = tempfile.NamedTemporaryFile()
 
 class Config(object):
     SECRET_KEY = 'REPLACE ME'
-    SERVER_NAME = 'appname'
 
 
 class ProdConfig(Config):
+    SERVER_NAME = 'replace me'
     ENV = 'prod'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     CACHE_TYPE = 'simple'
 
 
 class DevConfig(Config):
+    SERVER_NAME = 'localhost:5000'
     ENV = 'dev'
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     CACHE_TYPE = 'null'
     ASSETS_DEBUG = True
 
 
 class TestConfig(Config):
+    SERVER_NAME = 'localhost:5000'
     ENV = 'test'
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_file.name
     SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     CACHE_TYPE = 'null'
     WTF_CSRF_ENABLED = False
